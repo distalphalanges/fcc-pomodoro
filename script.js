c36957ed88c50f7bed7bfac932b0dd9dcc6f4f7d
distalphalanges <19227912+distalphalanges@users.noreply.github.com>
2018-02-11 17:48:25 +1100

    Fixed some bugs

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var modes = ['task','break','long'];
+var modes = ['task','rest','long'];
 
 var rhythms = {
 	'Pomodoro' : ['Pomodoro', 25,5,30,4],
@@ -8,20 +8,17 @@ var rhythms = {
 
 var app = {
 
-// handles all the application logic,
-// doesn't touch the DOM
-
-	init: function(rhythm, taskLength, breakLength, longBreakLength, numTasks) {
+	init: function(rhythm, taskLength, restLength, longBreakLength, numTasks) {
 
 		this.rhythm = rhythm || this.rhythm;
 		this.task = taskLength * 60 || this.task;
-		this.break = breakLength * 60 || this.break;
+		this.rest = restLength * 60 || this.rest;
 		this.long = longBreakLength * 60 || this.long;
-		this.numTasks = numTasks || 3;
+		this.numTasks = numTasks || 4;
 		this.currentTimer = new Timer('task', this.task);
 		this.timeLeft = this.task;
 		this.completed = this.completed || 0;
-		this.mode = 0;
+		this.resting = false;
 		this.running = false;
 
 		view.render();
@@ -57,24 +54,38 @@ var app = {
 		window.focus();
 	},
 
+	complete: function() {
+		this.completed++;
+		view.render();
+	},
+
+	switchMode: function(long) {
+		this.stop();
+		this.resting = +!this.resting;
+		if(long) {
+			this.currentTimer = new Timer('long', this.long);
+		} else {
+			this.currentTimer = new Timer(modes[+this.resting], this[modes[+this.resting]]);
+		}
+		view.render();
+		view.ding();
+	},
+
 	next: function() {
-		ding();
 		this.running = false;
-		switch(modes[this.mode]) {
+		switch(modes[+this.resting]) {
 
 			case 'task':
-				this.completed++;
+				this.complete();
 				if(this.completed === this.numTasks && this.numTasks >= 2) {
-					show(document.getElementById('complete'));
-					this.mode = 2;
-					this.currentTimer = new Timer('long', this.long);
+					view.show(document.getElementById('complete'));
+					this.switchMode(true);
 					this.start();
 					break;
 				}
 
-			case 'break':
-				this.mode = +!this.mode;
-				this.currentTimer = new Timer(modes[this.mode], this[modes[this.mode]]);
+			case 'rest':
+				this.switchMode();
 				this.start();
 				break;
 
@@ -82,30 +93,32 @@ var app = {
 
 	},
 
-	toggle: function(m) {
-		ding();
-		var buttons = document.querySelectorAll('.mode');
-		if(m != modes[this.mode]) {
-			if(this.mode < 2) {
-				if(buttons[this.mode].classList.contains('active')) {
-					buttons[this.mode].classList.remove('active');
-				}
-				buttons[+!this.mode].classList.add('active');
-				this.next();
-			} else {
-				show(document.getElementById('complete'));
-			}
-		}
-	},
-
 	stop: function() {
 		this.running = false;
 		this.currentTimer.stopTimer();
+	},
+
+	continue: function() {
+		this.numTasks += 4;
+		app.next();
+		view.hide(document.getElementById('complete'));
 	}
 
 };
 
 var view = {
+
+	hide: function(element) {
+		element.classList.add('hidden');
+	},
+
+	show: function(element) {
+		element.classList.remove('hidden');
+	},
+
+	ding: function() {
+		document.getElementById('ding').play();
+	},
 
 	render: function() {
 
@@ -113,7 +126,7 @@ var view = {
 			{ id: 'timeLeft', data: convert(app.timeLeft) },
 			{ id: 'numTasks', data: app.numTasks },
 			{ id: 'taskLength', data: convert(app.task) },
-			{ id: 'breakLength', data: convert(app.break) },
+			{ id: 'restLength', data: convert(app.rest) },
 			{ id: 'tasksCompleted', data: app.completed + ' task(s) completed'},
 			{ id: 'currentRhythm', data: app.rhythm },
 			{ id: 'selectRhythm', data: app.rhythm }
@@ -132,8 +145,16 @@ var view = {
 			pause: app.stop.bind(app),
 			reset: app.reset.bind(app),
 			apply: app.reset.bind(app),
-			toggleTask: app.toggle.bind(app, 'task'),
-			toggleBreak	: app.toggle.bind(app, 'break'),
+			toggleTask: function() {
+				if(app.resting) {
+					app.next();
+				}
+			},
+			toggleBreak	: function() {
+				if(!app.resting) {
+					app.next();
+				}
+			},
 			decreaseNumTasks: function() {
 				app.adjust('numTasks', -1);
 			},
@@ -147,24 +168,18 @@ var view = {
 				app.adjust('task', 60);
 			},
 			decreaseBreak: function() {
-				app.adjust('break', -60);
+				app.adjust('rest', -60);
 			},
 			increaseBreak: function() {
-				app.adjust('break', 60);
+				app.adjust('rest', 60);
 			},
 			showSettings: function() { 
-				show(document.getElementById('settings'));
+				view.show(document.getElementById('settings'));
 				view.render();
 			},
 			hideSettings: function() {
-				hide(document.getElementById('settings'));
+				view.hide(document.getElementById('settings'));
 				view.render();
-			},
-			continue: function() {
-				hide(document.getElementById('complete'));
-				app.toggle('task');
-				app.init();
-				app.start();
 			}
 		}
 
@@ -175,7 +190,7 @@ var view = {
 			'decreaseTask', 'increaseTask',	
 			'decreaseBreak', 'increaseBreak',
 			'showSettings', 'hideSettings',
-			'apply', 'continue'];
+			'apply'];
 
 		controls.forEach(function(control) {
 			document.getElementById(control)
@@ -194,41 +209,3 @@ var view = {
 
 view.bind();
 app.init.apply(app, rhythms['Pomodoro']);
-
-function hide(element) {
-	element.classList.add('hidden');
-}
-
-function show(element) {
-	element.classList.remove('hidden');
-}
-
-function ding() {
-	document.getElementById('ding').play();
-}
-
-function fadeOut(element) {
-  var opacity = 1;
-  var timer = setInterval(function () {
-    if (opacity <= 0.1){
-      clearInterval(timer);
-      hide(element);
-    }
-    element.style.opacity = opacity;
-    opacity -= opacity * 0.1;
-  }, 50);
-}
-
-function fadeIn(element) {
-  var opacity = 0;
-  element.style.opacity = opacity;
-  show(element);
-  
-  var timer = setInterval(function () {
-    if (opacity >= 1.0){
-      clearInterval(timer);
-    }
-    element.style.opacity = opacity;
-    opacity = opacity + 0.1;
-  }, 50);
-}
